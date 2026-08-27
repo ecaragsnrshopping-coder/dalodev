bc3f2a45bec5d632e247db9b1ea11241b15aa01d
ecarag <ecarag.snrshopping@gmail.com>
2026-08-27 14:10:07 +0800

    2

--- app/operators/include/management/userinfo.php
+++ app/operators/include/management/userinfo.php
@@ -34,7 +34,7 @@ if (strpos($_SERVER['PHP_SELF'], '/include/management/userinfo.php') !== false) 
 $_input_descriptors0 = array();
 
 $_input_descriptors0[] = array( 'id' => 'workphone',
-                                'caption' => t('ContactInfo','Employee ID'),
+                                'caption' => t('ContactInfo','WorkPhone1'),
                                 'type' => 'text',
                                 'value' => ((isset($ui_workphone)) ? $ui_workphone : ''),
                                 'name' => 'workphone' );
