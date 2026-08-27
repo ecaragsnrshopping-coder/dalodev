0e207ca5737c7c52f14b3e9f8be908840d80f7be
ecarag <ecarag.snrshopping@gmail.com>
2026-08-27 15:13:09 +0800

    2

--- app/operators/mng-list-all.php
+++ app/operators/mng-list-all.php
@@ -132,7 +132,7 @@
                      GROUP BY rc.username", $_SESSION['reportTable'], $_SESSION['reportQuery']);
     */
     // Fetch individual fields to concatenate in PHP
-    $sql = sprintf("SELECT ui.department, ui.firstname, ui.lastname, ui.workphone, ui.company,
+    $sql = sprintf("SELECT ui.department, ui.firstname, ui.lastname, ui.city, ui.company,
                            rc.username AS username, rc.value AS auth, rc.attribute
                      FROM %s %s
                      GROUP BY rc.username", $_SESSION['reportTable'], $_SESSION['reportQuery']);
